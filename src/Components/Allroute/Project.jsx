b5aecde076db687c1dfd9f569a6bab27178b3074
Mayur-Pc <sagar12500@gmail.com>
2022-10-29 13:19:23 +0530

    new projct add

--- src/Components/Allroute/Project.jsx
+++ src/Components/Allroute/Project.jsx
@@ -10,12 +10,30 @@ export const Project=()=>{
             <Text color="red" fontSize="2xl" >My Projects</Text>
             <hr style={{backgroundColor:"orange",width:"100%",height:"2px",marginBottom:"1rem"}}/>
 
+            <SimpleGrid columns={[1,1,2]} spacing='10px'>
+  <Box className="Box" width="100%"><img style={imgStyle} src="https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd337b26121202b5bfb96d7_OriginalSize(1).png"/></Box>
+  <Box textAlign="left" mt="1rem" ml="1rem" width="100%">
+    <Text fontSize="3xl" fontWeight="medium" color="white">My-hours Clone -Project</Text>
+    <Text fontSize="1xl" fontWeight="medium" color="pink">Tools: React || TypeScript || Chakra Ui || Redux || Node.js || MongoDB || Express ||</Text>
+    <Text color="yellow">My Hours is a cloud based time tracking software designed for small teams and freelancers. Track time on projects with tasks, generate insightful reports and send invoices to clients.A solo project excuted in 5 days.</Text>
+    <div style={btnStyle}>
+        <Button variant="outline" colorScheme="pink">
+           <a target="_blank" href="https://my-hourss.netlify.app/"><Text fontWeight="bold">Live</Text></a>
+            </Button>
+            <Button variant="outline" colorScheme="pink">
+           <a target="_blank" href="https://github.com/Sagarsandbhor17/Myhours-clone"><Text fontWeight="bold">Code</Text></a>
+            </Button>
+    </div>
+  </Box>
+</SimpleGrid>
+
 <SimpleGrid columns={[1,1,2]} spacing='10px'>
   <Box className="Box" width="100%"><img style={imgStyle} src="https://axiomq.com/wp-content/uploads/2020/11/fiverr-1.jpg"/></Box>
   <Box textAlign="left" mt="1rem" ml="1rem" width="100%">
     <Text fontSize="3xl" fontWeight="medium" color="white">Fiverr Clone -Project</Text>
     <Text fontSize="1xl" fontWeight="medium" color="pink">Tools: React || JavaScript || Chakra Ui</Text>
-    <Text color="yellow">A solo project excuted in 3 days.</Text>
+    <Text color="yellow">
+Fiverr connects entrepreneurs to experts to help them get every great idea done. Whether you need a variety of services to build your business from the ground up or one expert to complete one perfect job, Fiverr offers a world of creative freelancers.A solo project excuted in 3 days.</Text>
     <div style={btnStyle}>
         <Button variant="outline" colorScheme="pink">
            <a target="_blank" href="https://fiverr-clone-peach.vercel.app/"><Text fontWeight="bold">Live</Text></a>
